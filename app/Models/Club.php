5e8dad939556bd666c075706aa19a771ba9ac6d9
Andrew Narolsky <andrew.narolsky@gmail.com>
2026-04-26 10:51:08 +0300

    Added French & Georgian football clubs & competitions

--- app/Models/Club.php
+++ app/Models/Club.php
@@ -77,7 +77,7 @@ class Club extends Model
 
     public function getNormalizedNameAttribute(): string
     {
-        return preg_replace('/^(1.|AC|AF|AK|ASKÖ|CE|ČFK|DSV|FC|FCI|FF|FK|GNK|HNK|HŠK|IFK|JK|JS|K|KAA|KF|KRC|KS|KSC|KSK|KSV|KV|KVC|NK|OFC|PFC|PFK|POFC|R|RAA|RCS|RNK|RSC|RFC|RE|RRC|RWD|SC|SFC|SK|SKN|SV|SpC|TSV|UE)\s+/i', '', $this->name);
+        return preg_replace('/^(1.|AC|AF|AJ|AK|AS|ASKÖ|CA|CE|CO|ČFK|DSV|ÉF|EF|FC|FCI|FF|FK|GNK|HNK|HŠK|IFK|JK|JS|K|KAA|KF|KRC|KS|KSC|KSK|KSV|KV|KVC|LB|Le|NK|OFC|OGC|PFC|PFK|POFC|R|RAA|RC|RCS|RNK|RSC|RFC|RE|RRC|RWD|SC|SFC|SK|SKN|SV|SpC|TSV|UE|US)\s+/i', '', $this->name);
     }
 
     public function getCompetitionStatsAttribute(): Collection
